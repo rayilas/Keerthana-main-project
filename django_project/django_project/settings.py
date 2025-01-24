6bba4c72de3e55633fd7eee1af0018d1b1c8cee7
rayilas <keerthanaselvarajan4@gmail.com>
2025-01-24 16:04:20 +0530

    Admin_panel app has started

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates\Message Box")],
+        'DIRS': [os.path.join(BASE_DIR,"templates\Adminpanel")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
